51b67acc97e2f3d293f9c48ee663b21c0edfab94
Vince Kearney <vincent.kearney@saltdna.com>
2016-11-02 19:53:49 +0000

    Adding method for requesting the MLA JSON data

--- leaders/views.py
+++ leaders/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import urllib2
+import json
 
-# Create your views here.
+def fetchMlas(request):
+    req = urllib2.Request('http://data.niassembly.gov.uk/api/members/')
+    file = urllib2.build_opener().open(req)
+    jsonResp = json.loads(file.read())
+    return JsonResponse({ "response": jsonResp })
