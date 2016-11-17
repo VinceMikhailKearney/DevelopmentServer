7d3d7d55396aeedf964ddc9c85d3d63f4cb7af16
Vince Kearney <vincent.kearney@saltdna.com>
2016-11-17 16:21:10 +0000

    Added the elected-candidates.csv file and am now able to parse it on the server when requesting MLAs - Can find their twitter handle :)

--- leaders/views.py
+++ leaders/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
+from django.conf import settings
 import urllib2
 import json
+import csv
 
 def fetchMlas(request):
     req = urllib2.Request('http://data.niassembly.gov.uk/api/members/')
@@ -19,10 +21,20 @@ def fetchMlas(request):
         newDict['constituency'] = i['ConstituencyName']
         newDict['imageURL'] = i['MemberImgUrl']
         newDict['email'] = "test"
-        newDict['twitter'] = "test"
+        newDict['twitter'] = str(checkForTwitter(newDict['firstName'], newDict['lastName']))
         mlas_array.append(newDict)
 
     return JsonResponse({ "response": mlas_array })
+
+
+def checkForTwitter(first, last):
+    f = open('%s/elected-candidates.csv' % settings.BASE_DIR, 'r')
+    csv_f = csv.reader(f)
+
+    for row in csv_f:
+        if row[12].lower() == first.lower() and row[11].lower() == last.lower():
+            return row[13]
+
 
 def niamh(request):
     return JsonResponse({"Who does Vince love?": "Vince loves Niamh <3"})
