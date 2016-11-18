2307adf89d5daa4b845117c3c79f82cad7e80a99
Vince Kearney <vincent.kearney@saltdna.com>
2016-11-18 02:10:23 +0000

    We now parse and get twitter and email. This mean the entire MLA information is normalised and centralised in one source :) :) :)

--- leaders/views.py
+++ leaders/views.py
@@ -20,21 +20,25 @@ def fetchMlas(request):
         newDict['title'] = i['MemberTitle']
         newDict['constituency'] = i['ConstituencyName']
         newDict['imageURL'] = i['MemberImgUrl']
-        newDict['email'] = "test"
+        newDict['email'] = str(checkForEmail(newDict['firstName'], newDict['lastName']))
         newDict['twitter'] = str(checkForTwitter(newDict['firstName'], newDict['lastName']))
         mlas_array.append(newDict)
 
     return JsonResponse({ "response": mlas_array })
 
-
 def checkForTwitter(first, last):
+    return checkForData(first, last, 13)
+
+def checkForEmail(first, last):
+    return checkForData(first, last, 7)
+
+def checkForData(first, last, index):
     f = open('%s/elected-candidates.csv' % settings.BASE_DIR, 'r')
     csv_f = csv.reader(f)
 
     for row in csv_f:
         if row[12].lower() == first.lower() and row[11].lower() == last.lower():
-            return row[13]
-
+            return row[index]
 
 def niamh(request):
     return JsonResponse({"Who does Vince love?": "Vince loves Niamh <3"})
